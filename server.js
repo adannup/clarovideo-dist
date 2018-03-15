52c1b2ecc05084858b81ac873b79a499c8c1f7ad
adannup <zad3life@gmail.com>
2018-03-14 20:15:15 -0600

    Initial comment

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use('/', express.static('public'));
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server up on port ${PORT}`);
+});
